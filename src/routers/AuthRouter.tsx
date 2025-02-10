c1813df1e78b401e1dcbb5e98c8bad0ae9b4d873
coz <codesver@gmail.com>
2025-02-10 09:33:36 +0900

    fix : use outlet instead children

--- src/routers/AuthRouter.tsx
+++ src/routers/AuthRouter.tsx
@@ -1,11 +1,9 @@
-interface AuthRouterProps {
-  children?: React.ReactNode;
-}
+import { Outlet } from "react-router-dom";
 
-const AuthRouter = ({ children }: AuthRouterProps) => {
+const AuthRouter = () => {
   // TODO - check authentication and redirect if not authenticated.
 
-  return children;
+  return <Outlet />;
 };
 
 export default AuthRouter;
