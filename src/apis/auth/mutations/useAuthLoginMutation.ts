87acc73e4a15c92f0f78136a88ce7d2af46b5d61
coz <codesver@gmail.com>
2025-04-23 21:53:25 +0900

    refactor : move response io model to top level directory

--- src/apis/auth/mutations/useAuthLoginMutation.ts
+++ src/apis/auth/mutations/useAuthLoginMutation.ts
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 
 import { AuthLoginRequest } from "@apis/auth/models/AuthLogin";
 
-import { RestErrorResponse, RestResponse } from "@models/io/RestResponse";
+import { RestErrorResponse, RestResponse } from "@models/RestResponse";
 
 import rest from "@utils/rest";
 
