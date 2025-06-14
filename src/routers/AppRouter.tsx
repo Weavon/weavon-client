324a480fc700d1fafd34039faef6aaf24bd7b6a8
coz <codesver@gmail.com>
2025-06-14 23:20:21 +0900

    feat : change router to use data router instead Route component

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,8 +1,4 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import AuthGuard from "@/components/guard/AuthGuard";
 import UnAuthGuard from "@/components/guard/UnAuthGuard";
@@ -11,20 +7,38 @@ import HomePage from "@/pages/home/HomePage";
 import JoinPage from "@/pages/join/JoinPage";
 import LoginPage from "@/pages/login/LoginPage";
 
-const AppRouter = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route element={<UnAuthGuard />}>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/join" element={<JoinPage />} />
-      </Route>
-      <Route element={<AuthGuard />}>
-        <Route element={<PageLayout />}>
-          <Route path="/" element={<HomePage />} />
-        </Route>
-      </Route>
-    </Route>,
-  ),
-);
+const AppRouter = createBrowserRouter([
+  {
+    children: [
+      {
+        element: <UnAuthGuard />,
+        children: [
+          {
+            path: "/login",
+            element: <LoginPage />,
+          },
+          {
+            path: "/join",
+            element: <JoinPage />,
+          },
+        ],
+      },
+      {
+        element: <AuthGuard />,
+        children: [
+          {
+            element: <PageLayout />,
+            children: [
+              {
+                path: "/",
+                element: <HomePage />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 export default AppRouter;
